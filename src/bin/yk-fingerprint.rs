aa118c59da2ab45e132fb2a2d73f424eaf12cfc1
Mitchell Grenier <mitchell@confurious.io>
2021-01-14 17:18:34 -0500

    New tool extracts fingerprints from yubikey

--- src/bin/yk-fingerprint.rs
+++ src/bin/yk-fingerprint.rs
@@ -15,12 +15,21 @@ fn main() {
         return help();
     }
 
+    println!("Normal Slots:");
+    for slot in [0x9a, 0x9c, 0x9e, 0x9d, 0x9e, 0xf9].iter().map(|x| *x as u8) {
+        let s = SlotId::try_from(slot).unwrap();
+        match ssh_cert_fetch_pubkey(s) {
+            Some(cert) => println!("\t{:?}: Public Key Fingerprint: {}", s, cert.fingerprint().hash),
+            None => println!("\t{:?}: No cert found", s),
+        }
+    }
+
     println!("Retired Slots:");
     for slot in 0x82..0x95 as u8 {
         let s = RetiredSlotId::try_from(slot).unwrap();
         match ssh_cert_fetch_pubkey(SlotId::Retired(s)) {
-            Some(cert) => println!("\t{:?} Got Public Key: {}", s, cert.fingerprint().hash),
-            None => println!("\t{:?} No cert found", s),
+            Some(cert) => println!("\t{:?}: Public Key Fingerprint: {}", s, cert.fingerprint().hash),
+            None => println!("\t{:?}: No cert found", s),
         }
     }
 }
