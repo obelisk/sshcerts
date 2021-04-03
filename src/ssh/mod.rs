d985925c086a26ca075ba00d3e8a73e75a96e6b1
Mitchell Grenier <mitchell@confurious.io>
2021-04-03 13:47:21 -0400

    Change tooling to be FnOnce

--- src/ssh/mod.rs
+++ src/ssh/mod.rs
@@ -18,7 +18,7 @@ mod writer;
 
 /// This is a type that can be used for certificate signing when passed
 /// to Certificate::new
-pub type SigningFunction = Box<dyn Fn(&[u8]) -> Option<Vec<u8>> + Send + Sync>;
+pub type SigningFunction = Box<dyn FnOnce(&[u8]) -> Option<Vec<u8>> + Send + Sync>;
 
 pub use self::cert::{CertType, Certificate, CriticalOptions, Extensions};
 pub use self::keytype::{KeyType, KeyTypeKind, Curve, CurveKind};
