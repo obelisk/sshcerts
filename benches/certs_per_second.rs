94d6b9e9b70beb1c43a21bcd9af7b79b1971985e
Mitchell Grenier <mitchell@confurious.io>
2021-08-02 13:25:10 -0700

    Fix benchmark

---
+++ benches/certs_per_second.rs
@@ -0,0 +1,22 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use sshcerts::yubikey::{
+    RetiredSlotId,
+    SlotId,
+    Yubikey,
+};
+
+fn generate_certs(n: u64) -> () {
+    let data = [0; 32];
+    let mut yk = Yubikey::new().unwrap();
+    for _ in 0..n {
+        yk.ssh_cert_signer(&data, &SlotId::Retired(RetiredSlotId::R19)).unwrap();
+    }
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("generate 3 signatures", |b| b.iter(|| generate_certs(5)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
