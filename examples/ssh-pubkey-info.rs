b0941a08ac208ae190e309a4258ca7de6d8fe00b
Mitchell Grenier <mitchell@confurious.io>
2022-03-29 00:53:11 -0700

    New public key example

---
+++ examples/ssh-pubkey-info.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+use sshcerts::ssh::PublicKey;
+
+fn help() {
+    println!("An SSH Private Key reader based on the sshcerts library");
+    println!("Usage: ssh-pkey-info <path to file>");
+}
+
+fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 2 {
+        help();
+        return Ok(());
+    }
+
+    let path = &args[1];
+
+    match PublicKey::from_path(path) {
+        Ok(c) => {
+            println!("256 SHA256:{} {}", c.fingerprint().hash, c.comment.unwrap_or(format!("no comment")));
+            Ok(())
+        },
+        Err(e) => {
+            Err(format!("{}: Private key at {} not valid", e, &args[1]))
+        }
+    }
+}
