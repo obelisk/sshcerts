6361d77a256ee06bc0fdc645322f2394a078f232
Mitchell Grenier <mitchell@confurious.io>
2022-07-07 10:21:36 -0700

    Remove println

--- src/ssh/privkey.rs
+++ src/ssh/privkey.rs
@@ -230,10 +230,7 @@ impl super::SSHCertificateSigner for PrivateKey {
 
                 let keypair = match signature::RsaKeyPair::from_der(&asn_privkey) {
                     Ok(kp) => kp,
-                    Err(e) => {
-                        println!("{:?}", e);
-                        return None
-                    },
+                    Err(e) => return None,
                 };
 
                 let rng = rand::SystemRandom::new();
